6e78dff256abd9837ff498ec00f36935c7edc00b
Askar Akhmerov <askar.akhmerov@smava.de>
2016-04-17 13:00:33 +0200

    add processing of existing answers

--- ui/src/main/webapp/app/scripts/views/feedback/CategoriesView.js
+++ ui/src/main/webapp/app/scripts/views/feedback/CategoriesView.js
@@ -27,15 +27,21 @@ define([
         initialize: function (options) {
 //            nothing to do here
             this.options = options;
-            _.bindAll(this,'render','processQuestion','processPreview','showHideQuestion');
+            this.answeredQuestions = [];
+            this.collection = new CategoriesCollection();
+            _.bindAll(this,'render','processQuestion','processPreview','showHideQuestion','processActiveRow');
             if (localStorage.getItem('feedback')) {
                 this.model = new FeedbackModel(JSON.parse(localStorage.getItem('feedback')));
+                this.answersCollection = new AnswersCollection(this.model.get('answers'));
+                var answeredQuestions = this.answeredQuestions;
+                this.answersCollection.forEach(function (answer){
+                    answeredQuestions.push(answer.get('question'));
+                });
             } else {
                 this.model = new FeedbackModel();
+                this.answersCollection = new AnswersCollection();
             }
             this.categoryType = this.model.get('categoryType');
-            this.collection = new CategoriesCollection();
-            this.answersCollection = new AnswersCollection();
             $.when(this.collection.fetch()).then(this.render);
         },
 
@@ -46,7 +52,6 @@ define([
                 var model = new AnswerModel();
                 var answer = $(event.currentTarget).attr('answer');
                 var questionId = $(event.currentTarget).attr('question');
-                var questionNr = parseInt(questionId) + 1;
                 var question = this.collection.toJSON()[this.options.categoryId - 1].questions[questionId];
                 model.set('answer',answer);
                 model.set('question',questionId);
@@ -55,29 +60,38 @@ define([
                 if (this.categoryType == 1) {
                     model.set('text', this.$el.find('textarea[question="' + questionId + '"]').val());
                 }
-                this.answersCollection.add(model);
-                var activeRow =  this.$el.find('div[row-id="' + questionId + '"]');
-
-
-                var answerElement = '<div id="'+questionId+'" class="answers-question-block">' +
-                                        '<label>Frage:'+questionNr+'</label>' +
-                                        '<div>'+ question.question+'</div>' +
-                                        '<div class="text-center bottom col-xs-12">' +
-                                            '<button class="col-xs-3 col-xs-offset-0 btn pull-right editButton">Bearbeiten</button>'+
-                                        '</div>'
-                                    '</div>';
-                activeRow.after(answerElement);
-
-                activeRow.hide();
-
-
-
+                if (this.answeredQuestions.indexOf(questionId) < 0 ) {
+                    this.answersCollection.add(model);
+                }
+                //save it before navigate to the preview view!!
+                var fm = new FeedbackModel ();
+                fm.set('answers',this.answersCollection.toJSON());
+                fm.set('category',this.options.categoryId);
+                fm.set('categoryType',this.categoryType);
+                fm.save();
+                //persist information in local storage
+                localStorage.setItem('feedback', JSON.stringify(fm.toJSON()));
+                this.processActiveRow(questionId,question.question);
             } else {
                 $('div.error-box').css('display','block');
                 $('div.feedback-type').css('border','1px solid red');
                 $('div.feedback-type').addClass('error')
             }
 
+        },
+
+        processActiveRow : function (questionId,questionText) {
+            var questionNr = parseInt(questionId) + 1;
+            var activeRow =  this.$el.find('div[row-id="' + questionId + '"]');
+            var answerElement = '<div id="'+questionId+'" class="answers-question-block">' +
+                '<label> Frage:'+ questionNr + '</label>' +
+                '<div>'+ questionText+'</div>' +
+                '<div class="text-center bottom col-xs-12">' +
+                '<button class="col-xs-3 col-xs-offset-0 btn pull-right editButton">Bearbeiten</button>'+
+                '</div>' +
+            '</div>';
+            activeRow.after(answerElement);
+            activeRow.hide();
         },
 
         showHideQuestion:function(event) {
@@ -92,14 +106,7 @@ define([
             if(!this.answersCollection.length) {
                 return alert('please choose question')
             }
-            //save it before navigate to the preview view!!
-            var model = new FeedbackModel ();
-            model.set('answers',this.answersCollection.toJSON());
-            model.set('category',this.options.categoryId);
-            model.save();
-            model.set('categoryType',this.categoryType);
-            //persist information in local storage
-            localStorage.setItem('feedback', JSON.stringify(model.toJSON()));
+
 
             window.router.navigate("feedback/preview", {trigger: true});
         },
@@ -134,9 +141,10 @@ define([
             }
 
             this.$el.append(this.template(data));
-            if (this.model.get('answers')) {
+            if (this.model.get('answers') && this.collection.toJSON()[this.options.categoryId - 1]) {
                 for (var i = 0; i < this.model.get('answers').length; i++) {
-                    this.$el.find('div[row-id="' + this.model.get('answers')[i].question + '"]').hide();
+                    var answer = this.model.get('answers')[i];
+                    this.processActiveRow(answer.question,this.collection.toJSON()[this.options.categoryId - 1].questions[answer.question].question);
                 }
             }
             Backbone.Syphon.deserialize(this, this.model.toJSON());
